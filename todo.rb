27d3d0e01c3f36a1aacaea5088d649d8ee673ff5
David Kurutz <davidkurutz@gmail.com>
2016-03-01 01:17:13 -0500

    lots of work done

--- todo.rb
+++ todo.rb
@@ -1,7 +1,109 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'tilt/erubis'
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, 'secrset'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View all lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# Return an error message if the name is invalid.
+# Return nil if the name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    session[:error] = 'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    session[:error] = 'List name must be unique.'
+  end
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
+
+# show a single list
+get '/lists/:id' do
+  id = params[:id].to_i
+  @list =  session[:lists][id]
+  erb :list, layout: :layout
+end
+
+# edit an existing todo list.
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list =  session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+
+  id = params[:id].to_i
+  @list =  session[:lists][id]
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{id}"
+  end 
+end
+
+# Delete a list from session[:list] array
+post "/lists/:id/destroy" do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'The list has been deleted.'
+  redirect "/lists"
+end
+
+# Add a new todo to a list
+post "/lists/:list_id/todos" do
+  list_id = params[:list_id].to_i
+  @list = session[:lists][list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: text, completed: false }
+    session[:success] = 'The todo was added.'
+    redirect "/lists/#{list_id}"
+  end
 end
